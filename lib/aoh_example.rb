67e4121df14beabe51b4bcd8ce59a3da84f1ca0b
Samuel Escobar <sescobar8714@gmail.com>
2020-02-04 00:09:10 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,7 +24,9 @@ def literal_aoh
   new_arry = [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
     {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
     {:name=>"Pat Monteseian", :occupation=>"Staff"},
-    {:name=> "Kate Gompert", :occupation=> "None"}]
+    {:name=> "Kate Gompert", :occupation=> "None"},
+    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
